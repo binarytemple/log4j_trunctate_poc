15a1cc0eaa45c78b4c121cb7168f086032e46319
bryan hunt <github@binarytemple.co.uk>
2015-05-18 12:02:17 +0100

    working config

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+
+    private static Logger logger = Logger.getRootLogger();
+
+    public static void main(String[] args) {
+
+
+        try {
+            throw new Exception(new Exception("Inner Exception"));
+
+        }
+        catch(Throwable t){
+
+            logger.error("An error",t );
+        }
+
+
+
+
+
+
+
+    }
+}
